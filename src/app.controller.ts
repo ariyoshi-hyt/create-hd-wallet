e0b884fe31bcb68801b90a0292e3139a281a5d8c
ariyoshi-hyt <ariyoshi@mic9.co.jp>
2022-12-27 14:36:46 +0900

    fix: 軽微修正

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,7 +7,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello() {
+  createHdNode() {
     const mnemonic = ethers.Wallet.createRandom().mnemonic;
     const hdNode = ethers.utils.HDNode.fromMnemonic(mnemonic.phrase);
     const xprv = hdNode.extendedKey;
